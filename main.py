fe3c085bc7920d87f20a7a9ae692969bfe05eec0
Lee-Zzun <ulysses8755@gmail.com>
2024-08-31 12:18:20 +0900

    load_data 인터페이스 추가

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
 
 import streamlit as st
+from data_loader import load_data
 
 def main():
     st.title('This is Dashboard')
 
+    df = load_data()
+    
 if __name__ == '__main__':
     main()
