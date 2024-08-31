f82d84c7bd12689be98a8aa74f727d668e0f33ee
Lee-Zzun <ulysses8755@gmail.com>
2024-08-31 16:32:04 +0900

    주가 차트 추가

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ def main():
     df = load_data()
 
     # 특정 날짜 범위 선택
-    st.subheader('요일 범위 선택')
-    df['Date'] = pd.to_datetime(df['Date'])
-    start_date = st.date_input('시작일', df['Date'].min())
-    end_date = st.date_input('종료일', df['Date'].max())
+    with st.expander('범위를 선택하세요'):
+        col1, col2 = st.columns(2)
+        with col1:
+            start_date = st.date_input('시작일', df['Date'].min())
+        with col2:
+            end_date = st.date_input('종료일', df['Date'].max())
 
     range_df = df[(df['Date'] >= pd.to_datetime(start_date))
                    & (df['Date'] <= pd.to_datetime(end_date))]
     range_df = range_df.reset_index(drop=True)
 
-    st.table(range_df)
+    st.subheader('주가 변동 차트')
+    st.line_chart(range_df.set_index('Date')['Close'])
     
 if __name__ == '__main__':
     main()
